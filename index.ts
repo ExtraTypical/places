c3547ec71a7b3e043178f7c4df54ccb4fceac3de
ExtraTypical <contact@micahgray.me>
2023-09-28 15:30:30 -0400

    Working in TypeScript, so added index.ts

---
+++ index.ts
@@ -0,0 +1,13 @@
+interface Event {
+    // define the properties of the event object.
+}
+
+export function handler(event: Event) {
+    /**
+     * @summary Set up as if we were running this in AWS,
+     * though I'll probably change depending on where this
+     * is hosted.
+     */
+
+    
+}
